d19d2d8b7dddb8028114bc69a6eb5dd0823e22a5
ximenes <xresende@gmail.com>
2021-03-02 19:22:04 -0300

    Fix psdiag filters for SI corrs

--- images/applications/si-ps-corrs-c134-ia16t20.bash
+++ images/applications/si-ps-corrs-c134-ia16t20.bash
@@ -7,7 +7,7 @@
 /usr/local/bin/sirius-ioc-as-ps.py IA-20RaCtrl:CO-PSCtrl-SI4 &> /ioc-logs/sirius-ioc-si-ps-correctors-c134-ia20 &
 
 # Diag
-/usr/local/bin/sirius-ioc-as-ps-diag.py SI "(1[1-6]|20)C(1|3|4)" "C(H|V).*" &> /ioc-logs/sirius-ioc-si-ps-diag-corrs-c134-ia16t20 &
+/usr/local/bin/sirius-ioc-as-ps-diag.py SI "(1[6-9]|20)C(1|3|4)" "C(H|V).*" &> /ioc-logs/sirius-ioc-si-ps-diag-corrs-c134-ia16t20 &
 
 
 # keep entry point running
