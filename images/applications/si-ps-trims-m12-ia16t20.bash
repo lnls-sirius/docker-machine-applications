e5d827a18afa7562aa12209287f2c3ee7e83a978
anacso17 <anacso1777@gmail.com>
2021-03-03 14:36:29 -0300

    Fix diag filter in si-ps-trims-m12-ia16t20.bash

--- images/applications/si-ps-trims-m12-ia16t20.bash
+++ images/applications/si-ps-trims-m12-ia16t20.bash
@@ -9,7 +9,7 @@
 /usr/local/bin/sirius-ioc-as-ps.py IA-20RaCtrl:CO-PSCtrl-SI1 &> /ioc-logs/sirius-ioc-si-ps-trims-qs-m12-ia20 & # 35.0%
 
 # Diag
-/usr/local/bin/sirius-ioc-as-ps-diag.py SI "(1[1-6]|20)M(1|2)" "Q.*" &> /ioc-logs/sirius-ioc-si-ps-diag-trims-m12-ia16t20 & # 8.6%
+/usr/local/bin/sirius-ioc-as-ps-diag.py SI "(1[6-9]|20)M(1|2)" "Q.*" &> /ioc-logs/sirius-ioc-si-ps-diag-trims-m12-ia16t20 & # 8.6%
 
 # keep entry point running
 sleep infinity
