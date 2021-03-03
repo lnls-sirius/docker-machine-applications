7a00463998a3bfe09596d20bf077761659dac7cb
ximenes <xresende@gmail.com>
2021-03-03 10:33:02 -0300

    Update README and fix typo

--- images/applications/si-ps-sexts.bash
+++ images/applications/si-ps-sexts.bash
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # sirius-ioc-si-ps-sextupoles-sda12b2.service
-/usr/local/bin/sirius-ioc-as-ps.py PA-RaPSB06:CO-PSCtrl-SI1 &> /ioc-logs/sirius-ioc-si-ps-sda12b2  & # 11;9%
+/usr/local/bin/sirius-ioc-as-ps.py PA-RaPSB06:CO-PSCtrl-SI1 &> /ioc-logs/sirius-ioc-si-ps-sda12b2  & # 11.9%
 
 # sirius-ioc-si-ps-sextupoles-sfa0p0-sda0p0.service
 /usr/local/bin/sirius-ioc-as-ps.py PA-RaPSB02:CO-PSCtrl-SI2 &> /ioc-logs/sirius-ioc-si-ps-sfa0p0-sda0p0  & # 14.2%
