07a60470b95e59ac622dd61f887578b6103285d9
ximenes <xresende@gmail.com>
2021-03-03 08:45:41 -0300

    Fix missing space

--- images/applications/as-ap-currinfo.bash
+++ images/applications/as-ap-currinfo.bash
@@ -6,7 +6,7 @@
 /usr/local/bin/sirius-ioc-bo-ap-currinfo.py &> /ioc-logs/sirius-ioc-bo-ap-currinfo & # 3.6%
 /usr/local/bin/sirius-ioc-ts-ap-currinfo.py &> /ioc-logs/sirius-ioc-ts-ap-currinfo & # 3.6%
 /usr/local/bin/sirius-ioc-si-ap-currinfo.py &> /ioc-logs/sirius-ioc-si-ap-currinfo & # 4.4%
-/usr/local/bin/sirius-ioc-si-ap-currinfo-lifetime.py &> /ioc-logs/sirius-ioc-si-ap-currinfo-lifetime & #9.9%
+/usr/local/bin/sirius-ioc-si-ap-currinfo-lifetime.py &> /ioc-logs/sirius-ioc-si-ap-currinfo-lifetime & # 9.9%
 
 
 # keep entry point running
