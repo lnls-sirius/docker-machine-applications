35de8ec1c3b90dcafd7b3f35a9f482bb1d325b39
anacso17 <anacso1777@gmail.com>
2021-03-03 16:34:17 -0300

    Remove spare hyphen in bo-ps related files

--- images/applications/as-ps-dclinks-tbts-bodip.bash
+++ images/applications/as-ps-dclinks-tbts-bodip.bash
@@ -6,8 +6,8 @@
 /usr/local/bin/sirius-ioc-as-ps.py LA-RaCtrl:CO-DCLinkCtrl &> /ioc-logs/sirius-ioc-as-ps-dclink-tbts & # 4.8%
 
 # Dipole DCLink
-/usr/local/bin/sirius-ioc-as-ps.py PA-RaPSE05:CO-PSCtrl-BO2 &> /ioc-logs/sirius-ioc-bo--ps-dips-1-dclink & # 6.5%
-/usr/local/bin/sirius-ioc-as-ps.py PA-RaPSF05:CO-PSCtrl-BO2 &> /ioc-logs/sirius-ioc-bo--ps-dips-2-dclink & # 6.5%
+/usr/local/bin/sirius-ioc-as-ps.py PA-RaPSE05:CO-PSCtrl-BO2 &> /ioc-logs/sirius-ioc-bo-ps-dips-1-dclink & # 6.5%
+/usr/local/bin/sirius-ioc-as-ps.py PA-RaPSF05:CO-PSCtrl-BO2 &> /ioc-logs/sirius-ioc-bo-ps-dips-2-dclink & # 6.5%
 
 
 # keep entry point running
--- images/applications/bo-ps-dips.bash
+++ images/applications/bo-ps-dips.bash
@@ -3,11 +3,11 @@
 # CPU usage estimate (ps aux): 47.0%
 
 # Dipoles
-/usr/local/bin/sirius-ioc-as-ps.py PA-RaPSE05:CO-PSCtrl-BO1 &> /ioc-logs/sirius-ioc-bo--ps-dips-1 & # 21.5%
-/usr/local/bin/sirius-ioc-as-ps.py PA-RaPSF05:CO-PSCtrl-BO1 &> /ioc-logs/sirius-ioc-bo--ps-dips-2 & # 21.5%
+/usr/local/bin/sirius-ioc-as-ps.py PA-RaPSE05:CO-PSCtrl-BO1 &> /ioc-logs/sirius-ioc-bo-ps-dips-1 & # 21.5%
+/usr/local/bin/sirius-ioc-as-ps.py PA-RaPSF05:CO-PSCtrl-BO1 &> /ioc-logs/sirius-ioc-bo-ps-dips-2 & # 21.5%
 
 # Diagnostics
-/usr/local/bin/sirius-ioc-as-ps-diag.py BO ".*" "B-(1|2)" &> /ioc-logs/sirius-ioc-bo--ps-dips-diag & # 4.0%
+/usr/local/bin/sirius-ioc-as-ps-diag.py BO ".*" "B-(1|2)" &> /ioc-logs/sirius-ioc-bo-ps-dips-diag & # 4.0%
 
 
 # keep entry point running
