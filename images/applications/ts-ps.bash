693ab6afbea9ca95e5c7b32cb689afbbf49ec6c0
anacso17 <anacso1777@gmail.com>
2021-03-03 14:37:59 -0300

    Fix logfile name in ts-ps.bash

--- images/applications/ts-ps.bash
+++ images/applications/ts-ps.bash
@@ -11,7 +11,7 @@ sleep 2 # before starting other IOCs
 /usr/local/bin/sirius-ioc-as-ps.py LA-RaPS04:CO-PSCtrl-TS &> /ioc-logs/sirius-ioc-ts-ps-quadrupoles-34 & # 13.9%
 
 # Correctors
-/usr/local/bin/sirius-ioc-as-ps.py LA-RaCtrl:CO-PSCtrl-TS &> /ioc-logs/env-sirius-ioc-ts-ps-correctors & # 40.2%
+/usr/local/bin/sirius-ioc-as-ps.py LA-RaCtrl:CO-PSCtrl-TS &> /ioc-logs/sirius-ioc-ts-ps-correctors & # 40.2%
 
 # Diagnostics
 /usr/local/bin/sirius-ioc-as-ps-diag.py TS ".*" ".*" &> /ioc-logs/sirius-ioc-ts-ps-diag & # 6.3%
