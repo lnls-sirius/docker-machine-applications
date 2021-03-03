ddad63bfc1e294505e730bac31882ad4e49d63e2
anacso17 <anacso1777@gmail.com>
2021-03-03 14:40:16 -0300

    Fix diag filter in bo-ps-corrs-ia11t15.bash

--- images/applications/bo-ps-corrs-ia11t15.bash
+++ images/applications/bo-ps-corrs-ia11t15.bash
@@ -8,7 +8,7 @@
 /usr/local/bin/sirius-ioc-as-ps.py IA-14RaCtrl:CO-PSCtrl-BO &> /ioc-logs/sirius-ioc-bo-ps-correctors-ia14 & # 12.5%
 
 # Diagnostics
-/usr/local/bin/sirius-ioc-as-ps-diag.py BO "(2[7-9]|3[1-3]).*" "(CH|CV|QS)" &> /ioc-logs/sirius-ioc-bo-ps-correctors-diag-ia11t15 & # 3.8%
+/usr/local/bin/sirius-ioc-as-ps-diag.py BO "(2[7-9]|3[1-7]).*" "(CH|CV|QS)" &> /ioc-logs/sirius-ioc-bo-ps-correctors-diag-ia11t15 & # 3.8%
 
 # keep entry point running
 sleep infinity
