ca056e83e4f31986302c324ba428db317cff4086
ximenes <xresende@gmail.com>
2021-03-06 21:07:40 -0300

    Fix bash line

--- images/applications/si-ps-corrs-c2m12-ia02.bash
+++ images/applications/si-ps-corrs-c2m12-ia02.bash
@@ -2,7 +2,7 @@
 
 # CPU usage estimate (ps aux): 7.6%
 
-/usr/local/bin/sirius-ioc-as-ps.py IA-01RaCtrl:CO-PSCtrl-SI2 &> /ioc-logs/sirius-ioc-si-ps-corrs-c2m12-ia02 &
+/usr/local/bin/sirius-ioc-as-ps.py IA-02RaCtrl:CO-PSCtrl-SI2 &> /ioc-logs/sirius-ioc-si-ps-corrs-c2m12-ia02 &
 
 # Diag
 /usr/local/bin/sirius-ioc-as-ps-diag.py SI "02(C2|M1|M2)" "C(H|V).*" &> /ioc-logs/sirius-ioc-si-ps-diag-corrs-c2m12-ia02 & # 7.6%
