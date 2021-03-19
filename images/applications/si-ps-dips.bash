faa5e51363df962ad139f48c852aea60e0c305e0
ximenes <xresende@gmail.com>
2021-03-19 15:21:08 -0300

    Fix regexp replace mistake

--- images/applications/si-ps-dips.bash
+++ images/applications/si-ps-dips.bash
@@ -6,7 +6,7 @@
 /usr/local/bin/sirius-ioc-as-ps.py PA-RaPSD04:CO-PSCtrl-SI &> /ioc-logs/sirius-ioc-si-ps-dipoles.log & # 66.6%
 
 # Diag
-/usr/local/bin/sirius-ioc-as-ps-diag.py SI Fam "B1B2-(1|2)" &> /ioc-logs/sirius-ioc-si-ps-diag-dips .log & # 4.1%
+/usr/local/bin/sirius-ioc-as-ps-diag.py SI Fam "B1B2-(1|2)" &> /ioc-logs/sirius-ioc-si-ps-diag-dips.log & # 4.1%
 
 
 # run cron for log rotation
