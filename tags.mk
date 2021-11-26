75166b3b02e321c3414f6cdb5401e83809c51a0a
anacso17 <anacso1777@gmail.com>
2021-11-26 19:13:13 -0300

    Update tags for first official deploy

--- tags.mk
+++ tags.mk
@@ -10,11 +10,11 @@ EPICS_BASE_TAG ?= base-3.15.6
 
 # tags used for creating docker images
 IMG_DEBIAN_TAG ?= bullseye-20211011
-DEPLOY_TAG ?= 2021-10-18_test
+DEPLOY_TAG ?= 2021-11-29
 
 # image tags used for starting docker services
-IMG_IOCS_LI_PS_TAG ?= 2021-10-18_test
-IMG_PYTHON_TAG ?= 2021-10-18_test # also used as base image for fac-epics
-IMG_EPICS_TAG ?= 2021-10-18_test
-IMG_DEPS_TAG ?= 2021-10-18_test
+IMG_IOCS_LI_PS_TAG ?= 2021-11-29
+IMG_PYTHON_TAG ?= 2021-11-29 # also used as base image for fac-epics
+IMG_EPICS_TAG ?= 2021-11-29
+IMG_DEPS_TAG ?= 2021-11-29
 IMG_IOCS_TAG ?= $(DEPLOY_TAG)
