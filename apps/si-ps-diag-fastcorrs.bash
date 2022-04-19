4bf7a87e39f3564e21d28f15631b588ef992c893
Ana Clara Oliveira <ana.clara@lnls.br>
2022-04-19 17:53:07 -0300

    ENH: add si-ps-diag-fastcorrs service

---
+++ apps/si-ps-diag-fastcorrs.bash
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+export PYTHONUNBUFFERED=yes
+
+# Diag
+/usr/local/bin/sirius-ioc-as-ps-diag.py SI ".*" "FC.*" &> /ioc-logs/sirius-ioc-si-ps-diag-fastcorrs.log & #
+
+# run cron for log rotation
+cron &
+
+# keep entry point running
+sleep infinity
--- tools/generate_service_files.py
+++ tools/generate_service_files.py
@@ -158,6 +158,7 @@ class ServiceConfig:
         'si-ps-trims-qs-c1234-ia18': 'lnlsfac-srv2',
         'si-ps-trims-qs-c1234-ia19': 'lnlsfac-srv2',
         'si-ps-trims-qs-c1234-ia20': 'CA-RaTim-CO-IOCSrv',
+        'si-ps-diag-fastcorrs': 'IA-20RaDiag02-CO-IOCSrv-2',
         'it-ps-lens': 'lnlsfac-srv2',
         }
 
@@ -333,6 +334,7 @@ class ServiceConfig:
             'trims-qs-m12-ia18': ('si-ps-trims-qs-m12-ia18', ('dips', 'quads-qd', 'quads-qfq')),
             'trims-qs-m12-ia19': ('si-ps-trims-qs-m12-ia19', ('dips', 'quads-qd', 'quads-qfq')),
             'trims-qs-m12-ia20': ('si-ps-trims-qs-m12-ia20', ('dips', 'quads-qd', 'quads-qfq')),
+            'diag-fastcorrs': 'si-ps-diag-fastcorrs',
             },
         'it-ps': {
             'lens': 'it-ps-lens',
