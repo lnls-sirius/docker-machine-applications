c75c559b69396886553a9cf478045d51137cd718
ximenes <xresende@gmail.com>
2022-12-12 16:25:30 -0300

    Fix bugs

--- apps/si-id-epu50.bash
+++ apps/si-id-epu50.bash
@@ -5,12 +5,7 @@ export PYTHONUNBUFFERED=yes
 # CPU usage estimate (ps aux):
 
 /usr/local/bin/sirius-ioc-si-id-epu50.py \
-    --pv-prefix SI-10SB:ID-EPU50: \
-    --drive-io-port 5050 \
-    --drive-msg-port 5052 \
-    --beaglebone-addr 10.128.110.160 \
-    --autosave-dir /ioc-logs/autosave \
-    --request-file /ioc-logs/source/autosave_epu.req \
+    --pv-prefix SI-10SB:ID-EPU50:
 | tee /ioc-logs/sirius-ioc-si-id-epu50.log &
 
 # run cron for log rotation
