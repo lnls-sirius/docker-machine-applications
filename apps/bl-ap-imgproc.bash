bbe53f14e703161e078736f09928ac71bdbf7a7b
ximenes <xresende@gmail.com>
2023-03-21 20:17:42 -0300

    Update bl-ap-imgproc.bash

--- apps/bl-ap-imgproc.bash
+++ apps/bl-ap-imgproc.bash
@@ -4,8 +4,11 @@ export PYTHONUNBUFFERED=yes
 
 # CPU usage estimate (ps aux):
 
-/usr/local/bin/sirius-ioc-bl-ap-imgproc.py --devname CAX:B:BASLER01 \
-| tee /ioc-logs/sirius-ioc-bl-ap-imgproc.log &
+/usr/local/bin/sirius-ioc-bl-ap-imgproc-caxdvf2.py --devname CAX:B:BASLER01 \
+| tee /ioc-logs/sirius-ioc-bl-ap-imgproc-caxdvf2.log &
+
+# /usr/local/bin/sirius-ioc-bl-ap-imgproc-caxdvf1.py --devname CAX:A:BASLER01 \
+# | tee /ioc-logs/sirius-ioc-bl-ap-imgproc-caxdvf1.log &
 
 # run cron for log rotation
 cron &
